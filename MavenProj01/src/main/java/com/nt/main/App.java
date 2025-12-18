8ed2ade89cc675367fd924c2397d241eb2ae29a2
harshak999 <harshakmpohankar999@gmail.com>
2025-12-18 11:34:46 +0530

    initial commit

---
+++ MavenProj01/src/main/java/com/nt/main/App.java
@@ -0,0 +1,19 @@
+package com.nt.main;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+    	
+    	
+    	System.out.println("App Class....");
+    	
+    	int a=10;
+    	int b=20;
+    	int c=a+b;
+    	
+    	System.out.println("result  is ::"+c);
+    	
+    }
+}
